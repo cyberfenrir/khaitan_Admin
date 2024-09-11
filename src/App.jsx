b2a818a9970f8b980ea6e5c4c8b235e527017031
cyberfenrir <kumar.aayush04@gmail.com>
2024-09-11 19:31:50 +0530

    Initial Commit for admin proj

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import './App.css'; //hata dena base style hai
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Khaitan Admin</h1>
+    </div>
+  );
+}
+
+export default App;
