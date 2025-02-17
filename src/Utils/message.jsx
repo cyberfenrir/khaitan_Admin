d1282d532e1fda425af23c233d6df7a4580c6444
cyberfenrir <kumar.aayush04@gmail.com>
2025-02-17 17:36:22 +0530

    Message text fix

--- src/Utils/message.jsx
+++ src/Utils/message.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const MessageBox = ({ message, type, onClose }) => {
   return (
-    <div className={`flex items-center p-4 mb-4 text-sm rounded-lg shadow-lg ${type === 'error' ? ' text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400' : 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400'} text-white`}>
+    <div className={`flex items-center p-4 mb-4 text-sm rounded-lg shadow-lg ${type === 'error' ? ' text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400' : 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400'}`}>
       <div className="flex justify-between items-center">
         <span>{message}</span>
         <button onClick={onClose} className="ml-4 text-white">
